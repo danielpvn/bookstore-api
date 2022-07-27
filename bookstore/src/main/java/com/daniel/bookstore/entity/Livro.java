4acabbed7e1efda345a8fd5c77c4e43175575e01
danielpvn <daniel.pvn@hotmail.com>
2022-07-27 15:34:46 -0300

    Finalizando as entidades

--- bookstore/src/main/java/com/daniel/bookstore/entity/Livro.java
+++ bookstore/src/main/java/com/daniel/bookstore/entity/Livro.java
@@ -1,14 +1,30 @@
 package com.daniel.bookstore.entity;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import lombok.Data;
 
 @Data
-public class Livro {
+@Entity
+public class Livro implements Serializable {
  
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria; 
 }
